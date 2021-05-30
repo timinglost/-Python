1faa1af1ab706e3d06667a0856fd7725a75a1e3d
Kirill <budanovkirilldmitrievich@gmail.com>
2021-05-30 17:21:55 +0300

    dz

--- task2-4.py
+++
@@ -1,3 +0,0 @@
-def sum_of_elements(n, num):
-    return sum_of_elements(n - 1, (num / 2) * -1) + num if n > 0 else 0
-print(sum_of_elements(3, 1))
--- task2-7.py
+++
@@ -1,6 +0,0 @@
-def equality(n):
-    if n == 0:
-        return 0, False
-    else:
-        return equality(n - 1)[0] + n, equality(n - 1)[0] + n == (n * (n + 1) / 2)
-print(equality(int(input('Введите число: ')))[1])
